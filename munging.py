0e1aada9af44c144980af7c95ecadd1f1ffd778c
DSI <dsi@DSIs-Mac-mini-8.local>
2016-10-26 16:25:35 -0600

    add fb_published column

--- munging.py
+++ munging.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 
 def convert_data(df):
-    df = df[['sale_duration', 'previous_payouts', 'acct_type', 'channels', 'delivery_method', 'ticket_types', 'has_analytics', 'listed', 'num_order']]
+    df = df[['sale_duration', 'previous_payouts', 'acct_type', 'channels', 'delivery_method', 'ticket_types', 'user_type', 'fb_published', 'num_order']]
 
     #need to parse previous_payouts
     df['num_previous_payouts']= [len(row) for row in df['previous_payouts']]
@@ -24,7 +24,7 @@ def convert_data(df):
     df.drop(['acct_type'], axis = 1, inplace = True)
 
     #convert listed to boolean
-    df['listed'] = [val == 'y' for val in df['listed']]
+    # df['listed'] = [val == 'y' for val in df['listed']]
 
     # dummies = pd.get_dummies(df['delivery_method'], drop_first = True)
     df.fillna(inplace = True, value = 44.66)
